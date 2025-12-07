a6858df052f5540880a2a2a7fc2bcd9f4c36e619
VadimGordeev <vadimgordeev22@gmail.com>
2025-12-07 21:16:29 +0100

    feat: create view for cards

---
+++ Cards/Shapes.playground/Contents.swift
@@ -0,0 +1,305 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+        
+        let firstCardView = CardView<CircleShape>(
+            frame: CGRect(x: 0, y: 0, width: 120, height: 150),
+            color: .red
+        )
+        self.view.addSubview(firstCardView)
+        
+        let secondCardView = CardView<CircleShape>(
+            frame: CGRect(x: 200, y: 0, width: 120, height: 150),
+            color: .red
+        )
+        self.view.addSubview(secondCardView)
+        secondCardView.isFlipped = true
+        
+        //        //        round
+        //        view.layer
+        //            .addSublayer(
+        //                CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor)
+        //            )
+        //        //        square
+        //        view.layer
+        //            .addSublayer(
+        //                SquareShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor)
+        //            )
+        ////        cross
+        //        view.layer
+        //            .addSublayer(
+        //                CrossShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor)
+        //            )
+        ////        fill
+        //        view.layer.addSublayer(FillShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+    }
+    
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
+
+protocol ShapeLayerProtocol: CAShapeLayer {
+    init(size: CGSize, fillColor: CGColor)
+}
+
+extension ShapeLayerProtocol {
+    init() {
+        fatalError("init() has not been implemented")
+    }
+}
+
+class CircleShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+//        рассчитываем данные для круга, радиус равен половине меньшей из сторон
+        let radius = ([size.width, size.height].min() ?? 0) / 2
+//        центр круга равен центрам каждой из сторон
+        let centerX = size.width / 2
+        let centerY = size.height / 2
+        
+//        рисуем круг
+        let path = UIBezierPath(
+            arcCenter: CGPoint(x: centerX, y: centerY),
+            radius: radius,
+            startAngle: 0,
+            endAngle: .pi*2,
+            clockwise: true
+        )
+        path.close()
+//        инициализируем созданный путь
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class SquareShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+//        сторона равна меньшей из сторон
+        let edgeSize = ([size.width, size.height].min() ?? 0)
+//        рисуем квадрат
+        let rect = CGRect(x: 0, y: 0, width: edgeSize, height: edgeSize)
+        let path = UIBezierPath(rect: rect)
+        path.close()
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class CrossShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: size.width, y: size.height))
+        path.move(to: CGPoint(x: size.width, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: size.height))
+        self.path = path.cgPath
+        self.strokeColor = fillColor
+        self.lineWidth = 5
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class FillShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        for _ in 1...15 {
+//            координаты центра очередного круга
+            let randomX = Int.random(in: 0...Int(size.width))
+            let randomY = Int.random(in: 0...Int(size.height))
+            let center = CGPoint(x: randomX, y: randomY)
+//            смещаем указатель к центру круга
+            path.move(to: center)
+//            определяем случайный радиус
+            let radius = Int.random(in: 5...15)
+//            рисуем круг
+            path
+                .addArc(
+                    withCenter: center,
+                    radius: CGFloat(radius),
+                    startAngle: 0,
+                    endAngle: .pi*2,
+                    clockwise: true
+                )
+        }
+        
+        self.path = path.cgPath
+        self.fillColor = fillColor
+        self.strokeColor = fillColor
+        self.lineWidth = 1
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BackSideLine: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        for _ in 1...15 {
+            //            координаты начала очередной линии
+            let randomXStart = Int.random(in: 0...Int(size.width))
+            let randomYStart = Int.random(in: 0...Int(size.height))
+            //            координаты конца очередной линии
+            let randomXEnd = Int.random(in: 0...Int(size.width))
+            let randomYEnd = Int.random(in: 0...Int(size.height))
+            //            смещаем указатель к началу линии
+            path.move(to: CGPoint(x: randomXStart, y: randomYStart))
+            //            рисуем линию
+            path.addLine(to: CGPoint(x: randomXEnd, y: randomYEnd))
+        }
+        
+        self.path = path.cgPath
+        self.strokeColor = fillColor
+        self.lineWidth = 3
+        self.lineCap = .round
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+protocol FlippableView: UIView {
+    var isFlipped: Bool { get set }
+    var flipCompletionHandler: ((FlippableView) -> Void)? { get set }
+    func flip()
+}
+
+class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
+    
+    var isFlipped: Bool = false {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    var flipCompletionHandler: ((FlippableView) -> Void)?
+    func flip() {
+        <#code#>
+    }
+    
+    var color: UIColor!
+    var cornerRadius = 20
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.color = color
+        
+        setupBorders()
+    }
+    
+    private func setupBorders() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = CGFloat(cornerRadius)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    override func draw(_ rect: CGRect) {
+        backSideView.removeFromSuperview()
+        frontSideView.removeFromSuperview()
+        
+        if isFlipped {
+            self.addSubview(backSideView)
+            self.addSubview(frontSideView)
+        } else {
+            self.addSubview(frontSideView)
+            self.addSubview(backSideView)
+        }
+    }
+
+//    внутренний отступ представления
+    private let margin: Int = 10
+    
+//    представление с лицевой стороной карты
+    lazy var frontSideView: UIView = self.getFrontSideView()
+//    представление с обратной стороной карты
+    lazy var backSideView: UIView = self.getBackSideView()
+    
+    private func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        let shapeView = UIView(
+            frame: CGRect(
+                x: margin,
+                y: margin,
+                width: Int(self.bounds.width)-margin*2,
+                height: Int(self.bounds.height)-margin*2
+            )
+        )
+        view.addSubview(shapeView)
+        
+//        cоздание слоя с фигурой
+        let shapeLayer = ShapeType(size: shapeView.frame.size, fillColor: color.cgColor)
+        shapeView.layer.addSublayer(shapeLayer)
+        
+        return view
+    }
+    
+    private func getBackSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        
+        view.backgroundColor = .white
+        
+        switch["circle", "line"].randomElement()! {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BackSideLine(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        return view
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
